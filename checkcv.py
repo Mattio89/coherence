d1ee9c4b133cc6863e471975ff14613e681183bd
Mattio89 <matt.pitchford@gmail.com>
2017-09-22 16:18:04 -0500

    Cleaned up and explained the decisions in the code.

--- checkcv.py
+++ checkcv.py
@@ -11,7 +11,10 @@ filename = askopenfilename()
 
 with open(filename,'r', encoding="utf8") as inFile:
 	for line in inFile.readlines():
+		#This regular expression removes any of the "noise" from a .txt file containing the generated topics. Only the words in the topic remain.
 		line = re.sub('(Topic\w*)|[0-9.,()*:-]', '', line)
+		#Called in the NLTK tokenizer to help navigate outputing the correct lines.
 		words = WhitespaceTokenizer().tokenize(line)
-		#print(words[:10])
+		#Palmetto only takes the first 10 words in its webservice, so this limits the input and prints the coherence value
 		print(line, palmetto.get_coherence(words[:10]))
+		#I use the bash command 'python checkcv.py >> output.txt' to save the output.
